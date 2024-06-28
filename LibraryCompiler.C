779554b022b62a56d55d315e8f9f59c48feadea9
LandriWizard <matteo@LAPTOP-V8BT2AIF>
2024-06-28 14:34:40 +0200

    Caricato LibraryCompiler.C

---
+++ LibraryCompiler.C
@@ -0,0 +1,11 @@
+#include <Riostream.h>
+#include <TROOT.h>
+#include <TSystem.h> 
+
+void LibraryCompiler() {
+
+  //Librerie utili per la simulazione
+
+  gROOT->LoadMacro("MyPoint.cxx");
+  gROOT->LoadMacro("MyVertex.cxx");
+}
