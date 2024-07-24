119bb6e4f40d5e698549a408b3dfb69de11064a9
LandriWizard <matteo@LAPTOP-V8BT2AIF>
2024-07-24 13:12:49 +0200

    Aggiornato MyPhysics.h

--- MyPhysics.h
+++ MyPhysics.h
@@ -14,7 +14,14 @@ class MyPhysics : public TObject{
     MyPhysics(const MyPhysics& source);
     virtual ~MyPhysics();
 
+    double GetR() const {return dmR;}
+    double GetH() const {return dmH;}
+
+    void SetR(double r) {dmR = r;}
+    void SetH(double h) {dmH = h;}
+
     MyPoint Hit(MyPoint* Point, MyParticle* Particle);
+
 
   private:
     double dmR; //SQRT(X^2+Y^2)
