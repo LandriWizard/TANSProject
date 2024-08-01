64f5dbb72b4220c1d77f94ba4694b59238b067d6
LandriWizard <matteo.landrino@gmail.com>
2024-08-01 19:17:43 +0200

    Aggiunto smearing

--- MyPhysics.h
+++ MyPhysics.h
@@ -1,7 +1,9 @@
 #ifndef MYPHYSICS_H
 #define MYPHYSICS_H
 
-#include <TObject.h>
+#include "TObject.h"
+
+#include "MySignal.h"
 #include "MyParticle.h"
 #include "MyPoint.h"
 #include "MyRandom.h"
@@ -11,7 +13,7 @@ class MyPhysics : public TObject{
 
   public:
     MyPhysics();
-    MyPhysics(double R, double H, double scattering_theta);
+    MyPhysics(double R, double H, double scattering_theta, double smearing_z, double smearing_rphi);
     MyPhysics(const MyPhysics& source);
     virtual ~MyPhysics();
     MyPhysics& operator=(const MyPhysics& source);
@@ -29,12 +31,17 @@ class MyPhysics : public TObject{
     MyParticle NoScattering(MyParticle* Particle) {return *Particle;}
     MyParticle MultipleScattering(MyParticle* Particle);
 
+    MySignal NoSmearing(MySignal* Signal) {return *Signal;}
+    MySignal SmearingOn(MySignal* Signal);
+
 
 
   private:
     double dmR; //SQRT(X^2+Y^2)
     double dmH; //Lenght of the detectors
     double dmScatteringTheta; //scattering angle rms
+    double dmSmearingZ;
+    double dmSmearingRPhi;
 
   ClassDef(MyPhysics,1)
 
